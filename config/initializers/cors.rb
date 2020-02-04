137d305704192213586efc8d2e39cab1dba341dc
eddy <edv2@williams.edu>
2020-02-03 21:52:38 -0500

    all configuration is configured

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origins "https://localhost:3000"
+        resource "*", headers: :any, methods:[:get,:post,:put, :patch, :delete, :options, :head], credentials: true
+    end
+
+    #Where the production app is going to exist.
+    allow do
+        origins "https://tcc-scheduler-app-react.heroku.app.com"
+        resource "*", headers: :any, methods:[:get,:post,:put, :patch, :delete, :options, :head], credentials: true
+    end
+end
